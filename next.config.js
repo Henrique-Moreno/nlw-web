569589d56a5cf1a632467ffd9c7c7950360ce916
Henrique Moreno <henriqueprofissional26@gmail.com>
2023-05-19 13:55:38 -0300

    add files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+   images: {
+     domains: ['avatars.githubusercontent.com'],
+   },
+ }
