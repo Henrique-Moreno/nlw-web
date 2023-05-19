f0b65b861dee99fba608e02175504d67bc0a4544
Henrique Moreno <henriqueprofissional26@gmail.com>
2023-05-18 21:37:46 -0300

    add new script files

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function Copyright() {
+   return (
+      <div className="text-sm leading-relaxed text-gray-200">
+         Feito com 💜 no NLW da{' '}
+         <a
+            target="_blank"
+            rel="noopener"
+            className="underline"
+            href="https://rocketsea.com.br"
+         >
+            Rocketseat
+         </a>
+      </div>
+   )
+}
