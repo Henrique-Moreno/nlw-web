f9b1eb7d0b42f903917faa236f4526139bb42219
Henrique Moreno <henriqueprofissional26@gmail.com>
2023-05-20 22:29:26 -0300

    finishing mobile project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-     domains: ['avatars.githubusercontent.com'],
+     domains: ['avatars.githubusercontent.com', '192.168.1.7'],
    },
  }
